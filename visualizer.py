bc55b630e6e7a2bd9e4c48e92e339e30a3f59692
TerribleAtCreating <lucas4jeux@gmail.com>
2025-03-03 17:47:04 +0100

    finalized gui, cancel button

--- visualizer.py
+++ visualizer.py
@@ -2,10 +2,8 @@ try:
     import ffmpeg
     import numpy
     import matplotlib.pyplot as plt
-    import PIL
     from PIL import Image
     from PIL import ImageDraw
-    from alive_progress import alive_bar
 except ImportError:
     print("Import error! Please install any missing libraries, then restart the script.")
     quit()
@@ -17,6 +15,8 @@ import wave
 import re
 import os
 import ctypes
+import threading; from threading import Thread
+import timeit
     
 """
 yknow, when i was reading up on pysimplegui i didn't expect to have to pay 100 dollars
@@ -47,12 +47,20 @@ root.iconbitmap("pymv.ico", "pymv.ico")
 wave_name = tkinter.StringVar()
 export_name = tkinter.StringVar()
 
-framerate = tkinter.StringVar(value=60)
-bars = tkinter.StringVar(value=25)
-bar_spacing = tkinter.StringVar(value=5)
-lerp_alpha = tkinter.StringVar(value=.75)
-lerp_speed = tkinter.StringVar(value=25)
-background = tkinter.StringVar(value="greenscreen.png")
+defaultPreset = {
+    "framerate": 30,
+    "bars": 25,
+    "bar_spacing": 5,
+    "lerp_alpha": .75,
+    "lerp_speed": 25
+}
+
+framerate = tkinter.StringVar(value=defaultPreset.get("framerate"))
+bars = tkinter.StringVar(value=defaultPreset.get("bars"))
+bar_spacing = tkinter.StringVar(value=defaultPreset.get("bar_spacing"))
+lerp_alpha = tkinter.StringVar(value=defaultPreset.get("lerp_alpha"))
+lerp_speed = tkinter.StringVar(value=defaultPreset.get("lerp_speed"))
+background = tkinter.StringVar(value=defaultPreset.get("background"))
 
 def toSignalScale(signal):
     if signal < 1:
@@ -65,8 +73,8 @@ def check_num(newval):
 check_num_wrapper = (root.register(check_num), '%P')
 
 def check_float(newval):
-    return re.match('[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)', newval) is not None and len(newval) <= 5
-check_float_wrapper = (root.register(check_num), '%P')
+    return re.match('^[+-]?(?:[0-9]*[.])?[0-9]*$', newval) is not None and len(newval) <= 5
+check_float_wrapper = (root.register(check_float), '%P')
 
 # Dialog buttons
 def save_preset():
@@ -79,6 +87,7 @@ def save_preset():
             "bars": int(bars.get()),
             "bar_spacing": int(bar_spacing.get()),
             "lerp_alpha": float(lerp_alpha.get()),
+            "lerp_speed": float(lerp_speed.get()),
             "background": background.get()
             }, jsonOutput)
 def load_preset():
@@ -88,13 +97,14 @@ def load_preset():
     if not os.path.isfile(presetPath):
         tkinter.messagebox.showwarning("File not found", "Selected file does not exist.")
         return
-    preset = json.load(open(presetPath))
+    preset: dict = json.load(open(presetPath))
     
-    framerate.set(preset["framerate"])
-    bars.set(preset["bars"])
-    bar_spacing.set(preset["bar_spacing"])
-    lerp_alpha.set(preset["lerp_alpha"])
-    background.set(preset["background"])
+    framerate.set(preset.get("framerate", defaultPreset.get("framerate")))
+    bars.set(preset.get("bars", defaultPreset.get("bars")))
+    bar_spacing.set(preset.get("bar_spacing", defaultPreset.get("bar_spacing")))
+    lerp_alpha.set(preset.get("lerp_alpha", defaultPreset.get("lerp_alpha")))
+    lerp_speed.set(preset.get("lerp_speed", defaultPreset.get("lerp_speed")))
+    background.set(preset.get("background", defaultPreset.get("background")))
 
 elements = dict()
 # Layout: [(Element, X, Y, Widthspan, Heightspan)]
@@ -104,11 +114,11 @@ layout = [
     (tkinter.Label(root, text="Export filename:"), 0, 2, 1, 1), (tkinter.Entry(root, textvariable=export_name), 1, 2, 1, 1),
     
     (tkinter.Label(root, text="Render settings:"), 2, 0, 2, 1),
-    (tkinter.Label(root, text="Video framerate:"), 2, 1, 1, 1), (tkinter.Entry(root, textvariable=framerate, validatecommand=check_num_wrapper), 3, 1, 1, 1),
-    (tkinter.Label(root, text="Number of frequency bars to render:"), 2, 2, 1, 1), (tkinter.Entry(root, textvariable=bars, validatecommand=check_num_wrapper), 3, 2, 1, 1),
-    (tkinter.Label(root, text="Bar spacing (px):"), 2, 3, 1, 1), (tkinter.Entry(root, textvariable=bar_spacing, validatecommand=check_num_wrapper), 3, 3, 1, 1),
-    (tkinter.Label(root, text="Inbetween interpolation alpha:"), 2, 4, 1, 1), (tkinter.Entry(root, textvariable=lerp_alpha, validatecommand=check_float_wrapper), 3, 4, 1, 1),
-    (tkinter.Label(root, text="Interpolation rate:"), 2, 5, 1, 1), (tkinter.Entry(root, textvariable=lerp_speed, validatecommand=check_float_wrapper), 3, 5, 1, 1),
+    (tkinter.Label(root, text="Video framerate:"), 2, 1, 1, 1), (tkinter.Entry(root, textvariable=framerate, validate='key', validatecommand=check_num_wrapper), 3, 1, 1, 1),
+    (tkinter.Label(root, text="Number of frequency bars to render:"), 2, 2, 1, 1), (tkinter.Entry(root, textvariable=bars, validate='key', validatecommand=check_num_wrapper), 3, 2, 1, 1),
+    (tkinter.Label(root, text="Bar spacing (px):"), 2, 3, 1, 1), (tkinter.Entry(root, textvariable=bar_spacing, validate='key', validatecommand=check_num_wrapper), 3, 3, 1, 1),
+    (tkinter.Label(root, text="Inbetween interpolation alpha:"), 2, 4, 1, 1), (tkinter.Entry(root, textvariable=lerp_alpha, validate='key', validatecommand=check_float_wrapper), 3, 4, 1, 1),
+    (tkinter.Label(root, text="Interpolation rate:"), 2, 5, 1, 1), (tkinter.Entry(root, textvariable=lerp_speed, validate='key', validatecommand=check_float_wrapper), 3, 5, 1, 1),
     (tkinter.Label(root, text="Background image file:"), 2, 6, 1, 1), (tkinter.Entry(root, textvariable=background), 3, 6, 1, 1),
     
     (tkinter.Button(root, text="Save Preset", command=save_preset), 0, 7, 1, 1),
@@ -118,10 +128,17 @@ layout = [
 for widget, c, r, cs, rs in layout:
     widget.grid(column=c, row=r, columnspan=cs, rowspan=rs, padx=5, pady=5)
 
-continue_button = tkinter.Button(root, text="Continue")
-continue_button.grid(column=3, row=7, columnspan=1, rowspan=1, padx=5, pady=5)
+progressLabel = tkinter.Label(root, text="Ready")
+progressLabel.grid(column=1, row=8, columnspan=3, rowspan=3, padx=5, pady=5, sticky='w')
+continueButton = tkinter.Button(root, text="Render")
+continueButton.grid(column=3, row=7, columnspan=1, rowspan=1, padx=5, pady=5)
 progressBar = tkinter.ttk.Progressbar(root, orient="horizontal", mode="determinate", maximum=1)
 progressBar.grid(column=0, row=8, columnspan=1, rowspan=3, padx=5, pady=5)
+
+interrupted = False
+def interrupt():
+    global interrupted
+    interrupted = True
 
 # Main functions
 def render():
@@ -171,48 +188,51 @@ def render():
         .overwrite_output()
         .run_async(pipe_stdin=True)
     )
+    
+    renderStart = timeit.default_timer()
 
     prevSignals = [0] * barCount
-    interrupted = False
     progressBar.configure(maximum = frameCount - 1)
-    def interrupt():
-        interrupted = True
-    with alive_bar(math.floor(frameCount)) as bar:
-        def draw(frameNo: int):
-            bar()
-            progressBar['value'] = frameNo + 1
-            
-            frame = cImage.copy()  
-            draw = ImageDraw.Draw(frame)
-            # draw.rectangle((0, cImage.size[1] - 10, cImage.size[0], cImage.size[1] - 10), (255, 255, 255))
-
-            for j in range(0, barCount):
-                rawSignal = spectogram[0][round(j * freqMult)][round(frameNo * timeMult)]
-                adjustedLerp = 1 - math.pow(1 - lerp, lerpspeed/fps)
-                signal = toSignalScale(rawSignal) * adjustedLerp + prevSignals[j] * (1 - adjustedLerp)
-                prevSignals[j] = signal
-                size = abs(math.sqrt(signal / peak_signal) * imageY / 2)
-
-                draw.rectangle((imageX / barCount * j + barInterval, imageY - size, imageX / barCount * (j + 1) - barInterval, imageY), fill = (255, 255, 255))
-
-            process.stdin.write(
-                numpy.array(frame).tobytes()
-            )
-            
+    complete = threading.Event()
+    global interrupted; interrupted = False
+    def drawF(frameNo: int = 0):
+        if interrupted or frameNo >= frameCount:
+            complete.set()
+            return
         
-        continue_button.configure(text='Cancel', command=interrupt)
-        root.after(1)
-        for i in range(0, math.floor(frameCount)):
-            if interrupted:
-                break
-            draw(i)
-            root.after(1)
+        frame = cImage.copy()  
+        draw = ImageDraw.Draw(frame)
 
+        for j in range(0, barCount):
+            rawSignal = spectogram[0][round(j * freqMult)][round(frameNo * timeMult)]
+            adjustedLerp = 1 - math.pow(1 - lerp, lerpspeed/fps)
+            signal = toSignalScale(rawSignal) * adjustedLerp + prevSignals[j] * (1 - adjustedLerp)
+            prevSignals[j] = signal
+            size = abs(math.sqrt(signal / peak_signal) * imageY / 2)
+
+            draw.rectangle((imageX / barCount * j + barInterval, imageY - size, imageX / barCount * (j + 1) - barInterval, imageY), fill = (255, 255, 255))
+
+        process.stdin.write(
+            numpy.array(frame).tobytes()
+        )
+        progressBar['value'] = frameNo + 1
+        elapsed = timeit.default_timer() - renderStart
+        progressLabel.configure(text=f"Rendering... {elapsed:.1f}s elapsed - {frameNo + 1}/{frameCount} - {frameNo/elapsed:.1f}/s - {frameNo/elapsed/fps:.3f}x render speed")
+        root.after(1, lambda: drawF(frameNo + 1))
+        
+    
+    continueButton.configure(text='Cancel', command=interrupt)
+    
+    root.after(1, drawF)
+
+    complete.wait()
     process.stdin.close()
     process.wait()
     
     progressBar['value'] = 0
-    continue_button.configure(text='Continue', command=render)
+    continueButton.configure(text='Render', command=lambda: Thread(target=render).start())
+    elapsed = timeit.default_timer() - renderStart
+    progressLabel.configure(text=f"Finished in {elapsed:.1f}s - {frameCount}/{frameCount} - {frameCount/elapsed:.1f}/s - {frameCount/elapsed/fps:.3f}x render speed")
 
-continue_button.configure(command=render)
+continueButton.configure(command=lambda: Thread(target=render).start())
 root.mainloop()
